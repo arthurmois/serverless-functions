5bd5a258f3061a3c6509b3a119adb8af4c920b53
Arthur Mois <arthurmois@gmail.com>
2021-07-09 15:28:56 -0700

    revert

--- functions/util/hasura.js
+++ functions/util/hasura.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
 exports.hasuraRequest = async function({query,variables }) {
-    // const result = await fetch(process.env.HASURA_URL, {
-    const result = await fetch("https://serverless-egghead-arthur.hasura.app/v1/graphql", {
+    const result = await fetch(process.env.HASURA_URL, {
+    // const result = await fetch("https://serverless-egghead-arthur.hasura.app/v1/graphql", {
         method: 'POST',
         headers: {
             'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN_SECRET,
